6ad60c6dd7523f7008edcbb050eb04b462d9420c
Alok Dubey <143185228+alokdubey09@users.noreply.github.com>
2026-07-02 17:28:40 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (67.68%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+private: bool isMatched(char open, char close){
+    if((open == '(' && close == ')') ||
+       (open == '{' && close == '}') ||
+       (open == '[' && close == ']')
+       )
+       return true;
+
+       return false;
+}
+public:
+    bool isValid(string s) {
+        stack<char> st;
+       for(int i=0;i<s.length(); i++){
+        if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
+            st.push(s[i]);
+        }
+        else{
+            if(st.empty()) return false;
+            char ch = st.top();
+            st.pop();
+
+            if(!isMatched(ch, s[i]))
+            return false;
+        }
+       }
+       return st.empty();
+    }
+};
